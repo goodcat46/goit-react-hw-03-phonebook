9334772855df4e92b83ebe2c0eee3e57dd5abfe0
goodcat46 <107672708+goodcat46@users.noreply.github.com>
2022-10-13 19:43:00 +0300

    fix logic app.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,13 @@
-import css from './app.module.css';
 import React, { Component } from 'react';
+
 import Title from './Title/Title';
 import ContactForm from './ContactForm/ContactForm';
 import Filter from './Filter/Filter';
 import ContactList from './ContactList/ContactList';
+
 import localstorage from 'js/localstorage';
-// let contactsArr = [
-//   { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-//   { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-//   { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-//   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-// ];
-// // localstorage.save('contacts', contactsArr);
+
+import css from './app.module.css';
 
 export class App extends Component {
   state = {
@@ -25,27 +21,28 @@ export class App extends Component {
     return localstorage.load('contacts');
   };
   handleAddContact = ({ id, name, number }) => {
-    const namesArr = this.getContacts().map(el => el.name.toLowerCase());
     let contacts = this.getContacts();
+    const namesArr = contacts.map(el => el.name.toLowerCase());
     if (!namesArr.includes(name.toLowerCase())) {
       localstorage.save('contacts', [
         ...contacts,
         { id, name: name, number: number },
       ]);
       this.setState(() => ({
-        contacts: this.getContacts(),
+        contacts: contacts,
       }));
     } else {
       alert(`"${name}" is already added to contact list.`);
     }
   };
   handleDeleteContact = userId => {
+    let contacts = this.getContacts();
     localstorage.save(
       'contacts',
-      this.getContacts().filter(user => user.id !== userId)
+      contacts.filter(user => user.id !== userId)
     );
     this.setState(() => ({
-      contacts: this.getContacts(),
+      contacts: contacts,
     }));
   };
   handleChangeSearch = evt => {
@@ -55,11 +52,9 @@ export class App extends Component {
   applyFilter = () => {
     const { contacts, filter } = this.state;
 
-    return contacts.filter(({ name }) => {
-      if (filter && !name.toLowerCase().includes(filter.toLowerCase()))
-        return false;
-      return true;
-    });
+    return contacts.filter(({ name }) => 
+      !(filter && !name.toLowerCase().includes(filter.toLowerCase()))
+    );
   };
   componentDidMount() {
     this.setState({ contacts: this.getContacts() });
